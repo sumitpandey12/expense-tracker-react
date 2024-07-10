1c53435eb5ce556f98a9df972c83045a2215bd96
Sumit Pandey <heerjnv@gmail.com>
2024-07-10 10:46:05 +0530

    expense-item

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import ExpenseItem from './components/ExpenseItem'
+
+function App() {
+  return (
+    <div>
+      <h1>Let's get Started</h1>
+      <ExpenseItem></ExpenseItem>
+    </div>
+  );
+}
+
+export default App;
